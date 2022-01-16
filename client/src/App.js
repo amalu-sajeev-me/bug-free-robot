ab1e6952a887495350a88e23858d9a1ed9a089e6
ELISENWOLF <aravindprakash2001@gmail.com>
2022-01-17 02:31:45 +0530

    Landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import Home from './Components/Home/Home';
+import List from './Components/Home/List';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+    <List />
+    <Switch>
+      <Route path='/' component={Home}/>
+      {/* <Route path='/About' component={About}/>
+      <Route path='/Feature' component={Feature}/>
+      <Route path='/Contact' component={Contact Us}/> */}
+    </Switch>
+      <Footer/>
+    </>
   );
 }
 
