2f0f28ee355713f68230c3a0fc6e0d8508d4c1fe
ELISENWOLF <aravindprakash2001@gmail.com>
2022-01-17 20:37:13 +0530

    app.js error correction

--- client/src/App.js
+++ client/src/App.js
@@ -7,10 +7,6 @@ import List from './Components/Home/List';
 import Footer from './Components/Footer/Footer';
 import About from './Components/About/About';
 import Contactsection from './Components/Contactus/Contactsection';
-import Home from './Components/Home/Home';
-import Footer from './Components/Footer/Footer'
-import List from './Components/Home/List';
-import {Route} from 'react-router-dom';
 function App() {
   return (
     <>
