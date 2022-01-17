1b52af0b78e35fb949b51d92ddb98c9471248dfa
pranesh <pdhamande12.com>
2022-01-17 19:23:32 +0530

    final files

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import About from './Components/About/About';
 import Contactsection from './Components/Contactus/Contactsection';
-
+import Home from './Components/Home/Home';
+import Footer from './Components/Footer/Footer'
+import List from './Components/Home/List';
+import {Route} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <About/>
-      <Contactsection/>
+      <List/>
+      <Route exact path="/" component={Home}/>
+      <Route exact path="/about" component={About}/>
+      <Route exact path="/contact" component={Contactsection}/>
+      <Footer/>
       
     </div>
   );
--- client/src/Components/About/About.js
+++ client/src/Components/About/About.js
@@ -3,15 +3,15 @@ import "./aboutSection.css";
 // import { Box } from "@mui/material";
 const About = () => {
   return (
-      <div className ="container">
+      <div className ="container1">
         <h1>About Us</h1> 
-         <div className="aboutSection">
+         <div className="aboutSection1">
              <h2>Our Visions</h2>
              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores sint voluptas est iusto quasi repudiandae quia magni aliquam officiis, incidunt error laborum, fuga nisi omnis cumque nulla vitae dolor eaque.</p>
              <h2>Our Technology</h2>
              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, officia laudantium. Ex nam eaque hic repudiandae a dolores cumque, dolorum, debitis suscipit nisi delectus nesciunt.</p>
          </div> 
-           <div className="aboutSectionGradient">
+           <div className="aboutSectionGradient1">
              <h2>Image</h2>  
              <img src="" alt="" />
 
