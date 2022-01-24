1925956c62feb05906357e62032a57b5dc7667d0
ELISENWOLF <aravindprakash2001@gmail.com>
2022-01-24 17:37:24 +0530

    login page design correction

--- client/src/Components/Login/Signin.js
+++ client/src/Components/Login/Signin.js
@@ -9,7 +9,7 @@ import LP_Header from "../Header/LP_Header";
 function Signin() {
   const history = useHistory();
   const [values, setValues] = useState({
-    fullname: "",
+    username: "",
     email: "",
     password: "",
   });
@@ -22,14 +22,14 @@ function Signin() {
       [e.target.name]: e.target.value
     });
   };
-  const v_fullname = 'test';
+  const v_username = 'test';
   const v_email = 'test@values.com';
   const v_password = 'test12345';
   const handlebtnClick = (e) => {
     e.preventDefault();
     setErrors(validation(values));
     console.log(values);
-    if(values.fullname !== v_fullname && values.email !== v_email && values.password !== v_password){
+    if(values.username !== v_username && values.email !== v_email && values.password !== v_password){
       
     }else{
       history.push('/dashboard')
@@ -49,13 +49,9 @@ function Signin() {
               <h2 className="SG-title">Welcome Back</h2>
             </div>
             <form className="SG-form-wrapper">
-              <div className="SG-fullname">
-                <Input label="Full Name" type="text" name="fullname" value={values.fullname} onChange={handleChange}/>
-                {errors.fullname && (<p className="SG-error">{errors.fullname}</p>)}
-              </div>
-              <div className="SG-signinemail">
-                <Input label='Email' type="email" name="email" value={values.email} onChange={handleChange}/>
-                {errors.email && (<p className="SG-error">{errors.email}</p>)}
+              <div className="SG-username">
+                <Input label="Username" type="text" name="username" value={values.username} onChange={handleChange}/>
+                {errors.username && (<p className="SG-error">{errors.username}</p>)}
               </div>
               <div className="SG-signinpassword">
                 <Input label='Password' type="password" name="password" value={values.password} onChange={handleChange}/>
