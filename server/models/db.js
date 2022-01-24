04e812070ff2babf1921e5f16f1fea4eabf219aa
Amalu Sajeev <amalu.sajeev.me@gmail.com>
2022-01-24 16:40:01 +0530

    database fallback string

--- server/models/db.js
+++ server/models/db.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
-const { DB_STRING } = process.env;
+const {
+  DB_STRING = "mongodb+srv://dev:passmein@cluster0.gsr2u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+} = process.env;
 
 const db = async () => {
   await mongoose.connect(DB_STRING).then(() => {
