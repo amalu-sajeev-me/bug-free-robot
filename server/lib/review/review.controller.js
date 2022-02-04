166d1385427a6662fb1ee9f65c463c91d388928a
Amalu Sajeev <amalu.sajeev.me@gmail.com>
2022-02-04 21:57:36 +0530

    review delete feature added

--- server/lib/review/review.controller.js
+++ server/lib/review/review.controller.js
@@ -9,26 +9,28 @@ async function addReview(request, response) {
   const { stars, feedback } = request.body;
   const { userID } = request.session;
   const session = await connection.startSession();
-  await session.withTransaction(async () => {
-    const owner = await User.findById(userID);
-    const review = new Review({ owner, stars, feedback });
-    owner.reviews.push(review);
-    await owner.save();
-    await review.save();
-  }).then(() => {
-    response.json(say(true, "succesfully added the review"))
-  });
+  await session
+    .withTransaction(async () => {
+      const owner = await User.findById(userID);
+      const review = new Review({ owner, stars, feedback });
+      owner.reviews.push(review);
+      await owner.save();
+      await review.save();
+    })
+    .then(() => {
+      response.json(say(true, "succesfully added the review"));
+      session.endSession();
+    });
 }
 
 async function getReviews(request, response) {
   const { userID } = request.params;
-  const user =
-    await User.findOne({ username: userID }, { reviews: true })
-      .populate("reviews");
+  const user = await User.findOne(
+    { username: userID },
+    { reviews: true }
+  ).populate("reviews");
   response.json(user);
 }
-
-
 
 async function updateReview(request, response) {
   const { id } = request.params;
@@ -42,8 +44,22 @@ async function updateReview(request, response) {
   response.json(say(...successMsg));
 }
 
-async function removeReview() {
-
+async function removeReview(request, response) {
+  const { id } = request.params;
+  const session = await connection.startSession();
+  const successMsg = [true, `review with id: ${id} deleted`];
+  await session
+    .withTransaction(async () => {
+      const { owner } = await Review.findByIdAndDelete(id);
+      const user = await User.findById(owner);
+      const index = user.reviews.indexOf(id);
+      index && user.reviews.splice(index, 1);
+      await user.save();
+    })
+    .then(() => {
+      session.endSession();
+      response.json(say(...successMsg));
+    });
 }
 
 
--- server/lib/review/review.router.js
+++ server/lib/review/review.router.js
@@ -1,11 +1,12 @@
 import { createRouterSchema, createRouter } from "../../utils/ExpressHelper.js";
-import { addReview, getReviews, updateReview } from "./review.controller.js";
+import { addReview, getReviews, removeReview, updateReview } from "./review.controller.js";
 import { isValidReview } from "./review.util.js";
 
 const routerSchema = createRouterSchema({
   all: ["get", "/:userID", getReviews],
   add: ["post", "/:userID/new", isValidReview, addReview],
-  update: ["patch", "/:id", updateReview]
+  update: ["patch", "/:id", updateReview],
+  delete: ["delete", "/:id", removeReview]
 });
 const reviewRouter = createRouter(routerSchema);
 
