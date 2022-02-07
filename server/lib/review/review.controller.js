0e9917fb960be1fc77da4cae7322c4bba3f9812b
Amalu Sajeev <amalu.sajeev.me@gmail.com>
2022-02-07 11:52:35 +0530

    activity log for review actions

--- server/lib/review/review.controller.js
+++ server/lib/review/review.controller.js
@@ -1,6 +1,8 @@
 import { say } from "../../utils/index.js";
 import { User } from "../member/member.mongo.js";
 import { Review } from "../review/review.mongo.js";
+import { Action } from "../actions/action.mongo.js";
+
 import mongoose from "mongoose";
 const { ObjectId } = mongoose.Schema.Types;
 const { connection } = mongoose;
@@ -8,6 +10,8 @@ const { connection } = mongoose;
 async function addReview(request, response) {
   const { stars, feedback } = request.body;
   const { userID } = request.session;
+  const action = [`review`, `added a review to ${userID}`];
+  const activity = Action.activity(...action);
   const session = await connection.startSession();
   await session
     .withTransaction(async () => {
@@ -16,6 +20,7 @@ async function addReview(request, response) {
       owner.reviews.push(review);
       await owner.save();
       await review.save();
+      await activity.save();
     })
     .then(() => {
       response.json(say(true, "succesfully added the review"));
@@ -35,17 +40,22 @@ async function getReviews(request, response) {
 async function updateReview(request, response) {
   const { id } = request.params;
   const { stars, feedback } = request.body;
+  const action = [`review`, `updated a review of ${id}`];
+  const activity = Action.activity(...action);
   const successMsg = [true, "succesfully updated the review"];
   const updatedReview = await Review.findOneAndUpdate(
     { _oid: id },
     { $set: { stars, feedback } }
   );
+  await activity.save();
   console.log(updatedReview);
   response.json(say(...successMsg));
 }
 
 async function removeReview(request, response) {
   const { id } = request.params;
+  const action = [`review`, `deleted a review to ${id}`];
+  const activity = Action.activity(...action);
   const session = await connection.startSession();
   const successMsg = [true, `review with id: ${id} deleted`];
   await session
@@ -55,6 +65,7 @@ async function removeReview(request, response) {
       const index = user.reviews.indexOf(id);
       index && user.reviews.splice(index, 1);
       await user.save();
+      await activity.save();
     })
     .then(() => {
       session.endSession();
